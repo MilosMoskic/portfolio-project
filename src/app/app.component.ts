3d14cf376a329ace0047f633c8632c29de049bab
Milos Moskic <milosmoskic147@gmail.com>
2024-11-04 15:57:47 +0100

    Created landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FrontPageComponent } from "./front-page/front-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FrontPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
