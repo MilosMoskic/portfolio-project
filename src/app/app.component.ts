fdf754e221ff99142838e9c42f0d299cc0c86338
Milos Moskic <milosmoskic147@gmail.com>
2024-11-06 11:12:20 +0100

    Added footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { FrontPageComponent } from "./front-page/front-page.component";
 import { ProjectsPageComponent } from "./projects-page/projects-page.component";
 import { EducationPageComponent } from './education-page/education-page.component';
 import { ExperiencePageComponent } from "./experience-page/experience-page.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FrontPageComponent, ProjectsPageComponent, EducationPageComponent, ExperiencePageComponent],
+  imports: [RouterOutlet, HeaderComponent, FrontPageComponent, ProjectsPageComponent, EducationPageComponent, ExperiencePageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
