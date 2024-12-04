1830d8fef97939835a8b056656cd32f1a36446e5
Milos Moskic <milosmoskic147@gmail.com>
2024-12-04 16:18:49 +0100

    changes to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { FrontPageComponent } from "./front-page/front-page.component";
-import { ProjectsPageComponent } from "./projects-page/projects-page.component";
+import { FrontPageComponent } from './front-page/front-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { EducationPageComponent } from './education-page/education-page.component';
-import { ExperiencePageComponent } from "./experience-page/experience-page.component";
-import { FooterComponent } from "./footer/footer.component";
-import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { ExperiencePageComponent } from './experience-page/experience-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent, FrontPageComponent, ProjectsPageComponent, EducationPageComponent, ExperiencePageComponent, FooterComponent, ContactFormComponent],
+  imports: [
+    HeaderComponent,
+    FrontPageComponent,
+    ProjectsPageComponent,
+    EducationPageComponent,
+    ExperiencePageComponent,
+    FooterComponent,
+    ContactFormComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio-project';
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,62 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  isNavActive = false;
+  isMenuOpen = false;
+  toggleIcon = 'fa-solid fa-bars';
 
-  toggleNav() {
-    this.isNavActive = !this.isNavActive;
-    console.log("isNavActive:", this.isNavActive); // Check if this logs on each click
+  selectedSection: string = 'front-page';
+
+  @ViewChild('front-page') frontPageSection!: ElementRef;
+  @ViewChild('projects') projectsPageSection!: ElementRef;
+  @ViewChild('education') ecucationPageSection!: ElementRef;
+  @ViewChild('experience') experiencePageSection!: ElementRef;
+  @ViewChild('contact') contactPageSection!: ElementRef;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.toggleIcon = this.isMenuOpen
+      ? 'fa-solid fa-xmark'
+      : 'fa-solid fa-bars';
+  }
+
+  showSection(section: string) {
+    this.selectedSection = section;
+    this.scrollToSection(section);
+  }
+
+  scrollToSection(section: string) {
+    let target: ElementRef;
+
+    switch (section) {
+      case 'front-page':
+        target = this.frontPageSection;
+        break;
+      case 'projects':
+        target = this.projectsPageSection;
+        break;
+      case 'education':
+        target = this.ecucationPageSection;
+        break;
+      case 'experience':
+        target = this.experiencePageSection;
+        break;
+      case 'contact':
+        target = this.contactPageSection;
+        break;
+      default:
+        return;
+    }
+
+    if (target) {
+      target.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
